a48183fecc502229eb3edd7d1a04fa73d55c1626
Vijendra <kuldeep.singh@ediiie.com>
2021-11-16 11:37:47 +0530

    First Unity Client Commit

---
+++ Assets/Code/Networking/NetworkClient.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+namespace Project.Networking
+{
+    public class NetworkClient : SocketIOComponent
+    {
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
